aec363a3cd906387dcf480133bbd5264a36bc7c7
Andrey Velikoredchanin <andy@andyhost.ru>
2013-11-07 15:51:29 +0400

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-GoogleDriveVotes::Application.config.secret_token = '31541190ddaf0964b03226890182d0822f77db8801b7aee8937c975c23322c8bd262246151fc17993b89013c9945333c8b2d09c041a245c9e1a59edc450b1a1c'
+GoogleDriveVotes::Application.config.secret_token = 'e93a0e05f5925aa2bb95f4c5c8ac4d04a2174602c27600c18f786ee8151006ace6ff14cd0817b329e28d1f8245884f456aca814d8d02196a2155e6216f206cfb'
