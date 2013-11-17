ab094c4d4a65795cf9c2aa3a96249d6be81dc24c
Andrey Velikoredchanin <andy@andyhost.ru>
2013-11-17 20:38:16 +0400

    Fix

--- config/initializers/fix_ssl.rb
+++ config/initializers/fix_ssl.rb
@@ -21,7 +21,7 @@ module GoogleDrive
           http.use_ssl = true
           http.verify_mode = OpenSSL::SSL::VERIFY_NONE
           ca_file_rpmos = "/etc/pki/tls/certs/ca-bundle.crt"
-          if File.exist?(ca_file_rpmos)
+          if FileTest.exists?(ca_file_rpmos)
             Rails.logger.info("DBG: Set CA file for SSL")
             http.ca_file = ca_file_rpmos
           end
