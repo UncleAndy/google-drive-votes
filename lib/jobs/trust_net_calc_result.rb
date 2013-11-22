c09aab52579d62671bad5d05ebb1c4f0c095c30a
Andrey Velikoredchanin <andy@andyhost.ru>
2013-11-22 21:17:40 +0400

    Logger for rescue scheduler job

--- lib/jobs/trust_net_calc_result.rb
+++ lib/jobs/trust_net_calc_result.rb
@@ -3,6 +3,7 @@ class Jobs::TrustNetCalcResult
   @queue = :trust_net_calc_result
 
   def self.perform
+    Rails.logger.info("[RESCUE SCHEDULER JOB] TrustNetCalcResult run")
     TrustNet.calculate
   end
 end
