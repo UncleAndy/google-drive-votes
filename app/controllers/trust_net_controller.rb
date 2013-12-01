b8748ba2e2b20f29d581b951b083b1957b6d9ac6
Andrey Velikoredchanin <andy@andyhost.ru>
2013-12-01 22:39:57 +0400

    Show last time calculation for trust net results

--- app/controllers/trust_net_controller.rb
+++ app/controllers/trust_net_controller.rb
@@ -17,6 +17,10 @@ class TrustNetController < ApplicationController
     else
       @results = TrustNetResult.order('verify_level desc')
     end
+
+    @last_time_calculate = '-'
+    ltc = TrustNetResultHistory.maximum(:result_time)
+    @last_time_calculate = ltc.localtime.strftime("%F %T") if ltc.present?
     
     respond_to do |format|
       format.html
